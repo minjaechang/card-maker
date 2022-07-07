8926cf1d7e75fe571dfa2eb7509799243824ac7e
Minjae Chang <minjaejchang@gmail.com>
2022-07-08 00:10:10 +1200

    Add login component UI

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import styles from './footer.module.css';
 
 const Footer = (props) => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.title}>Code your dream</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import styles from './login.module.css';
 
 const Login = ({ authService }) => {
   const onLogin = (e) => {
@@ -10,16 +11,20 @@ const Login = ({ authService }) => {
   };
 
   return (
-    <section>
+    <section className={styles.login}>
       <Header />
       <section>
         <h1>Login</h1>
-        <ul>
-          <li>
-            <button onClick={onLogin}>Google</button>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              Google
+            </button>
           </li>
-          <li>
-            <button onClick={onLogin}>GitHub</button>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              GitHub
+            </button>
           </li>
         </ul>
       </section>
