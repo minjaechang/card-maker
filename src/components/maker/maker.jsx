b0ad00ca8c6e595823d36802e73261ec13cc4bd5
Minjae Chang <minjaejchang@gmail.com>
2022-07-09 20:12:09 +1200

    Add editor and preview layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -3,6 +3,8 @@ import Header from '../header/header';
 import Footer from '../footer/footer';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
   const navigate = useNavigate();
@@ -22,6 +24,10 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
