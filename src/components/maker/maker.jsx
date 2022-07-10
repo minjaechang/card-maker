f7a228687800766d43c196b970c5d9943f950b52
Minjae Chang <minjaejchang@gmail.com>
2022-07-10 15:15:25 +1200

    add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -5,8 +5,44 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
+import { useState } from 'react';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'light',
+      title: 'Software engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it!',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Software engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it!',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'colorful',
+      title: 'Software engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it!',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
+    },
+  ]);
   const navigate = useNavigate();
 
   const onLogout = () => {
@@ -25,8 +61,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
